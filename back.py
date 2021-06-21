a40657d658b9b2d877d54b4e27fef9a5d15b8582
Aysegul Koc <aysegulkoc@Ayseguls-MacBook-Pro.local>
2021-06-21 15:26:19 +0300

    first save

---
+++ back.py
@@ -0,0 +1,47 @@
+import numpy as np
+from cv2 import cv2 as cv
+import matplotlib.pyplot as plt
+
+cap = cv.VideoCapture('/Users/aysegulkoc/Movies/3_01.mp4')
+# kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3,3))
+kernel = np.ones((10, 10), np.uint8)
+# fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
+fgbg = cv.bgsegm.createBackgroundSubtractorGMG()
+# fgbg = cv.createBackgroundSubtractorMOG2(detectShadows=True)
+# fgbg = cv.createBackgroundSubtractorKNN(detectShadows=True)
+liste = []
+while True:
+    ret, old_frame = cap.read()
+    frame = old_frame[550:850, 250:1150]
+
+    # frame = old_frame[400:760, 639:1111]
+    if frame is None:
+        break
+    fgmask = fgbg.apply(frame)
+    fgmask = cv.morphologyEx(fgmask, cv.MORPH_CLOSE, kernel)
+    fgmask = cv.morphologyEx(fgmask, cv.MORPH_CLOSE, kernel)
+    fgmask = cv.morphologyEx(fgmask, cv.MORPH_CLOSE, kernel)
+
+    (h, w) = fgmask.shape
+    total_pix = h * w
+
+    # black_pix = np.sum(fgmask == 0)
+    white_pix = np.sum(fgmask == 255)
+
+    metric = white_pix / total_pix
+    liste.append(metric)
+
+    cv.imshow('Frame', frame)
+    cv.imshow('FG MASK Frame', fgmask)
+
+    keyboard = cv.waitKey(30)
+    if keyboard == 'q' or keyboard == 27:
+        break
+
+plt.plot(liste)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
+
+cap.release()
+cv.destroyAllWindows()
---
+++ roi.py
@@ -0,0 +1,23 @@
+from cv2 import cv2
+
+# load original
+# img = cv2.imread("ornek.png")
+
+# img_crop = img[583:1290, 750:1350]
+# frame = old_frame[639:623, 1081:1082] 2_01.mp4
+
+cap = cv2.VideoCapture('/Users/aysegulkoc/Movies/2_01.mp4')
+
+while True:
+    ret, old_frame = cap.read()
+    frame = old_frame[595:850, 200:1150]
+    cv2.circle(frame, (430, 80), 20, (0, 0, 255), -1)
+
+    # cv2.imshow('Frame', old_frame)
+    cv2.imshow('Frame', frame)
+    keyboard = cv2.waitKey(30)
+
+    if keyboard == 'q' or keyboard == 27:
+        break
+# cv2.imshow("normal image", img)
+cv2.destroyAllWindows()
